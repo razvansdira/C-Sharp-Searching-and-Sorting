82c7a6687210d62420cc762880d5154bb6b6d9cd
Razvan Sdira <sdira_razvan@yahoo.com>
2016-11-26 10:36:13 +0200

    Add files via upload

---
+++ Exercise7.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Searching_and_Sorting
+{
+    class Exercise7
+    {
+        static void Main(string[] args)
+        {
+            List<int> unsorted = new List<int>();
+            List<int> sorted;
+
+            Random random = new Random();
+
+            Console.Write(" Original array: ");
+            for (int i = 0; i < 10; i++)
+            {
+                unsorted.Add(random.Next(0, 100));
+                Console.Write(unsorted[i] + " ");
+            }
+
+            sorted = MergeSort(unsorted);
+
+            Console.Write("\n   Sorted array: ");
+            foreach (int x in sorted)
+                Console.Write(x + " ");
+            Console.ReadLine();
+        }
+
+        private static List<int> MergeSort(List<int> unsorted)
+        {
+            if (unsorted.Count <= 1)
+                return unsorted;
+
+            List<int> left = new List<int>();
+            List<int> right = new List<int>();
+
+            int middle = unsorted.Count / 2;
+
+            for (int i = 0; i < middle; i++)  
+                left.Add(unsorted[i]);
+
+            for (int i = middle; i < unsorted.Count; i++)
+                right.Add(unsorted[i]);
+
+            left = MergeSort(left);
+            right = MergeSort(right);
+
+            return Merge(left, right);
+        }
+
+        private static List<int> Merge(List<int> left, List<int> right)
+        {
+            List<int> result = new List<int>();
+
+            while (left.Count > 0 || right.Count > 0)
+            {
+                if (left.Count > 0 && right.Count > 0)
+                {
+                    if (left.First() <= right.First())  
+                    {
+                        result.Add(left.First());
+                        left.Remove(left.First());      
+                    }
+                    else
+                    {
+                        result.Add(right.First());
+                        right.Remove(right.First());
+                    }
+                }
+                else if (left.Count > 0)
+                {
+                    result.Add(left.First());
+                    left.Remove(left.First());
+                }
+                else if (right.Count > 0)
+                {
+                    result.Add(right.First());
+                    right.Remove(right.First());
+                }
+            }
+            return result;
+        }
+    }
+}
